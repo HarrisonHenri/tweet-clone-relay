6bde51d215ec1ed486f7c17ae1ee1f353c82ccb5
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-03 18:44:58 -0300

    feat: add components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+import React, { Suspense } from 'react';
+import { ActivityIndicator, SafeAreaView, StatusBar } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
+import { AppColors } from './styles/global';
+import { StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <Suspense fallback={<ActivityIndicator />}>
+            <Routes />
+          </Suspense>
+        </SafeAreaView>
+      </NavigationContainer>
+    </>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: { flex: 1, backgroundColor: AppColors.GRAY },
+});
