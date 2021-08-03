fed2de44c17de91ced41c0a2750fc3254510ae83
HarrisonHenri <harrisonhenrisn@gmail.com>
2021-08-03 17:59:54 -0300

    fix: fix app name typo

--- ios/tweetclonelrelayTests/tweetclonelrelayTests.m
+++ ios/tweetclonelrelayTests/tweetclonelrelayTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface tweetclonelrelayTests : XCTestCase
+@interface tweetclonerelayTests : XCTestCase
 
 @end
 
-@implementation tweetclonelrelayTests
+@implementation tweetclonerelayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
